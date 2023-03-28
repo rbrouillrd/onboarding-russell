1fc70fa5e765052d87daefe03517d44ecc9a1e37
rbrouillard <rbrouillard@squadle.com>
2023-03-28 14:58:35 -0600

    save dd feilds

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=20, null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.title
